380a6984c7b3460e6d9d358fc91211eb726f10c6
anishghanekar <anish@ghanekar.com>
2020-11-25 13:06:49 +0000

    harvard cpu

---
+++ rtl/mips_cpu/mips_cpu_harvard.v
@@ -0,0 +1,18 @@
+module mips_cpu_harvard(
+    
+    input logic clk,
+    input logic reset,
+    output logic active,
+    output logic[31:0] register_v0,
+    
+    input logic clk_enable,
+    
+    output logic[31:0] instr_address,
+    input logic[31:0] instr_readdata,
+    
+    output logic[31:0] data_address,
+    output logic data_write,
+    output logic data_read,
+    output logic[31:0] data_writedata,
+    input logic[31:0] data_readdata,
+);
