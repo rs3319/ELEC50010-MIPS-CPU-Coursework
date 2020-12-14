e8570d681ca8b14d39f65e9dfbce4b34e75724ce
anishghanekar <anish@ghanekar.com>
2020-12-14 14:49:10 +0530

    added multiplication defs to hilo

--- rtl/mips_cpu/mips_cpu_hilo.v
+++ rtl/mips_cpu/mips_cpu_hilo.v
@@ -1,24 +1,41 @@
 module mips_cpu_hilo(
 
+    input logic[5:0] hilo_control,
+    input logic[5:0] opcode,
     input logic clk,
     input logic reset,
-
-    input logic[4:0] read_index_rs,
-    output logic[31:0] read_data_rs,
-    input logic[4:0] read_index_rt,
-    output logic[31:0] read_data_rt,
+    input logic[31:0] a,
+    input logic[31:0] b,
     
-    input logic[7:0] write_index,
-    input logic write_enable,
-    input logic[31:0] write_data_lo,
-    input logic[31:0] write_data_hi,
+    
     output logic[31:0] lo_reg,
     output logic[31:0] hi_reg
 );
+    logic[31:0] temp;
+    logic[63:0] toadd;
     logic[63:0] prodreg;
     logic[31:0] lo;
     logic[31:0] hi;
+    integer i;
 
     
 // remove register signals and only include hi/lo reg and multiplicand multiplier
+
+always @(posedge clk) begin
+    for(i = 0; i < 32; i = i + 1) begin
+        temp = a * b[i];
+        toadd = temp<<i;
+        if(i == 0) begin
+            prodreg = toadd;
+        end
+        else begin
+            prodreg = prodreg + toadd;
+        end
+    end
+    hi[31:0] = prodreg[63:32];
+    lo[31:0] = prodreg[31:0];
+    
+    hi_reg = hi;
+    lo_reg = lo;
+end
 endmodule
