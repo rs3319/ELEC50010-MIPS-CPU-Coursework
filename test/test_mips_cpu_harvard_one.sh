df3721b7d69e0581d2e32d5764349b302840be6e
Alden0012 <Aldenjsk2001@gmail.com>
2020-12-18 19:03:33 +0000

    Added JALR testcases

--- test/test_mips_cpu_harvard_one.sh
+++ test/test_mips_cpu_harvard_one.sh
@@ -10,7 +10,7 @@ REFS_OUT=$(cat "test/Outputs/${BN}.txt")
 #Compile Everything in directory
 iverilog -g 2012 -s mips_cpu_harvard_tb  \ test/mips_cpu_harvard_tb.v test/mips_cpu_dMemory.v test/mips_cpu_iMemory.v ${DIRECTORY}/mips_cpu_harvard.v ${DIRECTORY}/mips_cpu/*.v \  -Pmips_cpu_harvard_tb.RAM_INIT_FILE=\"${TC}\" -Pmips_cpu_harvard_tb.REF_OUT=${REFS_OUT} \  -o test/Executables/mips_cpu_harvard_tb_${BN} 
 set +e
-test/Executables/mips_cpu_harvard_tb_${BN} > warning.log
+test/Executables/mips_cpu_harvard_tb_${BN} > warnings.log
 RESULT=$?
 set -e
 
